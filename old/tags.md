044db681823f7092be2ed3d8bfb7bdeeaf785be5
Chris Hodgson <chris.3.hodgson@plus.net>
2019-06-19 16:11:29 +0100

    initial migration from php site

---
+++ old/tags.md
@@ -0,0 +1,7 @@
+---
+layout: tags
+title: Post Tags
+date: 2018-03-03
+---
+
+<h4>Post Tags</h4>
---
+++ twig/portfolio.php
@@ -0,0 +1,76 @@
+<?php
+
+$portfolio = [
+    'waterman-mews' => [
+        'current_project' => true,
+        'completion_date' => 'September 2019',
+        'address' => 'Reading Road, Henley-on-Thames.',
+        'description' =>
+            '<p>An exclusive development of traditional Mews style properties situated off the Reading Road, within walking distance to the town centre, train station and river, also within the highly regarded Trinity School catchment area.</p>'
+    ],
+    'berkshire-road' => [
+        'completion_date' => '2019',
+        'address' => 'Berkshire Road, Henley-on-Thames.',
+        'description' =>
+            '<p>Set in one of the most desirable locations, within walking distance of the Centre of Henley-on-Thames is this bespoke development, comprising a pair of semi-detached and one detached family homes.</p>'
+            . '<p>Designed to suit the surrounding area in a Victorian style, incorporating many feature synonymous with a Jamie Smith Estates property. Each dwelling has been created to meet the needs of modern lifestyles with open plan kitchen / living / dining space and separate formal Drawing room.</p>'
+            . '<p>The properties will be provided with off road parking and West facing rear gardens.</p>'
+    ],
+    'hickman-and-hayman-house' => [
+        'completion_date' => '2017',
+        'address' => 'St Andrews Road, Henley-on-Thames.',
+        'description' =>
+            '<p>Two new build properties, constructed in the traditional late Victorian style, typical to Henley-on-Thames. Situated on one of Henley’s prime residential roads, the properties are in a great location to take advantage of all Henley’s amenities including the river and the train station.</p>'
+            . '<p>Built in the customary Jamie Smith Estates style, the properties have many interesting features, including detailed Flemish bond brickwork, bay windows, decorative porches and the signature Jamie Smith Estates barge boards. An extra special feature of these two properties are the gargoyle finial detail.</p>'
+            . '<p>Jamie Smith Estates received a nominations for the Premier Guarantee and LABC building</p>'
+    ],
+    'ferry-cottage' => [
+        'completion_date' => '2016',
+        'address' => 'Lashbrook Road, Lower Shiplake.',
+        'description' =>
+            '<p>Ferry Cottage is an outstanding detached property standing on the banks of the river Thames in Shiplake. The property was constructed to replace the original ferryman’s cottage, which unfortunately was destroyed by flooding in 2014.</p>'
+            . '<p>Due to the close proximity to the river, ground soil conditions and access restrictions, this build was extremely challenging. The new build property draws on design characteristics from the original dwelling, mixed with the traditional Jamie Smith Estates style.</p>'
+            . '<p>Key features include detailed brickwork arches and plinths, box and splayed bay windows, decorative timber detailing and balustrade. Jamie Smith Estates achieved Highly Commended status at the 2016 Premier Guarantee excellence awards for this project.</p>'
+    ],
+    'farm-road' => [
+        'completion_date' => '2014',
+        'address' => 'Farm Road, Henley-on-Thames.',
+        'description' =>
+            '<p>The Farm Road development gave Jamie Smith Estates the opportunity to extend and renovate Overington Cottage and construct two new two bedroom cottages in the same early Victorian style.The existing dwelling, believed to be the original farm house was completely renovation and enlarged with a single storey orangery extension.</p>'
+            . '<p>The new cottages were constructed to replicate the original building with slate roofs, detailed block render elevations, large detailed soffits and wrought iron porches.</p>'
+    ],
+    'newtown-villa' => [
+        'completion_date' => '2014',
+        'address' => 'Newtown Gardens, Henley-on-Thames.',
+        'description' =>
+            '<p>Newtown Villa is a 4 bedroom detached property, situated in a quiet residential lane, close to the centre of Henley. The property was the first dwelling to be completed in our current style incorporating stone window cills and heads, buff brick detailing and the signature Jamie Smith Estates barge boards.</p>'
+             . '<p>Jamie Smith Estates received a nomination for the Premier Guarantee Excellence awards, for this project.</p>'
+    ],
+    'hilchoice' => [
+        'completion_date' => '2012',
+        'address' => 'Stoke Row Road, Peppard Common.',
+        'description' =>
+            '<p>Hilchoice is a detached Victorian Villa, built in the early 1900’s, formally the village shop and post office, the property had been in the same family ownership since its construction.</p>'
+            . '<p>The property was extended and fully renovated to a high standard and included a magnificent open plan kitchen reception with Victorian roof Lantern.</p>'
+    ],
+    'belverdere' => [
+        'completion_date' => '2007',
+        'address' => 'Rotherfield Road, Henley-on-Thames.',
+        'description' =>
+            '<p>A new build large detached Victorian style property, constructed on Henley’s premier road, situated on a beautiful sloping site with views over the Harpsden valley. A stunning property constructed with outdoor pool, waterfall and Jacuzzi.</p>'
+    ],
+    'peppard-lodge' => [
+        'completion_date' => '??',
+        'address' => '??',
+        'description' =>
+            '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan ligula sed suscipit pellentesque. Aliquam erat volutpat. Quisque ut accumsan erat. Curabitur faucibus commodo bibendum. Vestibulum id euismod odio. Proin a tempor quam. </p>'
+            . '<p>Cras tincidunt, purus vel congue dapibus, dolor nibh vestibulum lorem, vitae accumsan nunc turpis id tortor. Integer molestie egestas felis, et ornare diam tempus sed. Curabitur at ornare leo. Nulla facilisi.</p>'
+    ],
+    'the-pines' => [
+        'completion_date' => '??',
+        'address' => '??',
+        'description' =>
+            '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus accumsan ligula sed suscipit pellentesque. Aliquam erat volutpat. Quisque ut accumsan erat. Curabitur faucibus commodo bibendum. Vestibulum id euismod odio. Proin a tempor quam. </p>'
+            . '<p>Cras tincidunt, purus vel congue dapibus, dolor nibh vestibulum lorem, vitae accumsan nunc turpis id tortor. Integer molestie egestas felis, et ornare diam tempus sed. Curabitur at ornare leo. Nulla facilisi.</p>'
+    ],
+];
